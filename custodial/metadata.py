853cde6cce6fa5e442388868839aca804d46a21d
Eric Johnson <ebjpdx@users.noreply.github.com>
2017-01-06 15:50:48 -0800

    adding database to store application data

---
+++ custodial/metadata.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Table, Column, Integer, String, Date, MetaData
+
+metadata = MetaData()
+
+bookmarks = Table('bookmarks', metadata,
+                  Column('id', Integer, primary_key=True),
+                  Column('url', String, nullable=False),
+                  Column('first_week_observed', Date),
+                  Column('latest_week_observed', Date),
+                  Column('weeks_observed', Integer)
+                  )
+
+bookmark_exclusions = Table('bookmark_exclusions', metadata,
+                            Column('id', Integer, primary_key=True),
+                            Column('url_pattern', String, nullable=False),
+                            Column('pattern_type', String, nullable=False)
+                            )
